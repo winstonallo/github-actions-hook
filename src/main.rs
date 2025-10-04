af3895d592f732d1045bbf3cd63af1a4723149e5
winstonallo <arthurbiedchar@gmail.com>
2025-10-04 19:09:43 +0200

    Run commands and aggregate failures (not working yet)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+#![allow(unused)]
+
 use saphyr::{LoadableYamlNode, Yaml};
 use std::collections::BTreeMap;
-use std::env;
-use std::fs::File;
 use std::io::prelude::*;
+use std::process::Command;
+use std::{fs, io};
 
 #[derive(Debug)]
 struct Event {
     branches: Option<Vec<String>>,
     paths: Vec<String>,
+}
+
+#[derive(Debug)]
+struct Step {
+    name: String,
+    command: String,
 }
 
 #[derive(Debug)]
@@ -22,107 +30,93 @@ struct GithubActionWorkflow {
     jobs: BTreeMap<String, Job>,
 }
 
-fn print_indent(indent: usize) {
-    for _ in 0..indent {
-        print!("{}   ", indent);
-    }
-}
-
-fn dump_node(doc: &Yaml, indent: usize) {
-    match *doc {
-        Yaml::Sequence(ref v) => {
-            for x in v {
-                dump_node(x, indent + 1);
-            }
-        }
-        Yaml::Mapping(ref h) => {
-            for (k, v) in h {
-                print_indent(indent);
-                if v.contains_mapping_key("<<") {
-                    println!("{}:", k.as_str().unwrap());
-                    dump_node(v.as_mapping_get("<<").unwrap(), indent + 1);
-                } else {
-                    println!("{}:", k.as_str().unwrap());
-                    dump_node(v, indent + 1);
-                }
-            }
-        }
-        _ => {
-            print_indent(indent);
-            println!("{}", doc.as_str().unwrap());
-        }
-    }
-}
-
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    let mut f = File::open(&args[1]).unwrap();
+fn parse_action(path: &str) -> GithubActionWorkflow {
+    let mut f = fs::File::open(path).unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
 
     let docs = Yaml::load_from_str(&s).unwrap();
-    let workflow = {
-        let mut name = String::new();
-        let mut on = BTreeMap::<String, Event>::new();
-        let mut jobs = BTreeMap::<String, Job>::new();
-        let Yaml::Mapping(doc) = &docs[0] else { unreachable!() };
-        for (k, v) in doc {
-            match k.as_str() {
-                Some(s) => match s {
-                    "name" => name = s.to_owned(),
-                    "on" => {
-                        on.insert(
-                            s.to_owned(),
-                            Event {
-                                branches: Some(Vec::new()),
-                                paths: Vec::new(),
-                            },
-                        );
-                    }
-                    "jobs" => {
-                        let Yaml::Mapping(jbs) = v else { panic!("'jobs' value is not a mapping") };
-                        for (job_name, v) in jbs {
-                            let Yaml::Mapping(job) = v else { unreachable!() };
-                            for (k, v) in job {
-                                match k.as_str() {
-                                    Some(s) => match s {
-                                        "steps" => {
-                                            let Yaml::Sequence(steps) = v else { unreachable!() };
-                                            for step in steps {
-                                                match step.as_mapping_get("run") {
-                                                    Some(command) => {
-                                                        if jobs.get(job_name.as_str().unwrap()).is_none() {
-                                                            jobs.insert(
-                                                                job_name.as_str().unwrap().to_owned(),
-                                                                Job {
-                                                                    steps: Vec::from([command.as_str().unwrap().to_owned()]),
-                                                                },
-                                                            );
-                                                        };
-                                                    }
-                                                    None => {}
-                                                };
-                                            }
-
-                                            println!("{v:?}");
+    let mut name = String::new();
+    let mut on = BTreeMap::<String, Event>::new();
+    let mut jobs = BTreeMap::<String, Job>::new();
+    let Yaml::Mapping(doc) = &docs[0] else { unreachable!() };
+    for (k, v) in doc {
+        if let Some(s) = k.as_str() {
+            match s {
+                "name" => name = s.to_owned(),
+                "on" => {
+                    on.insert(
+                        s.to_owned(),
+                        Event {
+                            branches: Some(Vec::new()),
+                            paths: Vec::new(),
+                        },
+                    );
+                }
+                "jobs" => {
+                    let Yaml::Mapping(jbs) = v else { panic!("'jobs' value is not a mapping") };
+                    for (job_name, v) in jbs {
+                        let Yaml::Mapping(job) = v else { unreachable!() };
+                        for (k, v) in job {
+                            match k.as_str() {
+                                Some(s) => {
+                                    if s == "steps" {
+                                        let Yaml::Sequence(steps) = v else { unreachable!() };
+                                        for step in steps {
+                                            if let Some(command) = step.as_mapping_get("run")
+                                                && !jobs.contains_key(job_name.as_str().unwrap())
+                                            {
+                                                jobs.insert(
+                                                    job_name.as_str().unwrap().to_owned(),
+                                                    Job {
+                                                        steps: Vec::from([command.as_str().unwrap().to_owned()]),
+                                                    },
+                                                );
+                                            };
                                         }
-                                        _ => {}
-                                    },
-                                    _ => unreachable!(),
+                                    }
                                 }
+                                _ => unreachable!(),
                             }
                         }
-                        // jobs.insert(s.to_owned(), Job { steps: Vec::new() });
                     }
-                    _ => unreachable!(),
-                },
-                None => {}
+                }
+                _ => unreachable!(),
             }
-
-            // dump_node(doc, 0);
         }
-        GithubActionWorkflow { name, on, jobs }
-    };
+    }
+    GithubActionWorkflow { name, on, jobs }
+}
 
-    println!("{workflow:?}")
+struct Failure {
+    command: String,
+    exit_code: i32,
+    stderr: String,
+    stdout: String,
+}
+
+fn main() -> io::Result<()> {
+    let run_id = uuid::Uuid::new_v4().to_string();
+    let mut workflows = Vec::new();
+    for file in fs::read_dir(".github/workflows")? {
+        workflows.push(parse_action(file.unwrap().path().to_str().unwrap()));
+    }
+    let mut failures = Vec::new();
+    for workflow in workflows {
+        for job in workflow.jobs {
+            for step in job.1.steps {
+                let stdout_path = format!("/tmp/{run_id}_{}")
+                println!("Running {step}");
+                let mut command = Command::new("sh");
+                command.arg("-c").arg(&step);
+                let mut child = command.output()?;
+                let Some(exit_code) = child.status.code() else { panic!("No exit code") };
+                println!("Exit status: {exit_code}");
+                if exit_code != 0 {
+                    failures.push(Failure { command: step, exit_code });
+                }
+            }
+        }
+    }
+    Ok(())
 }
